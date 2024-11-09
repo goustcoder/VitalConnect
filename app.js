c7e39f6508316808e582666b7e4c9850d46b06b8
jeet_mandal <jeet22761@gmail.com>
2024-11-09 13:37:05 +0530

    Npm implimented

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+
+app.listen(8080, () => {
+    console.log("Server is listening on port 3000");
+});
+
+app.get('/',(req,res)=>{
+    res.render('home.ejs');
+})
