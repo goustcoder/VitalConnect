2c077a8a8d2506930d10b0b94b04e419237f62de
jeet_mandal <jeet22761@gmail.com>
2024-11-09 22:27:35 +0530

    update

--- public/js/home.js
+++ public/js/home.js
@@ -3,3 +3,13 @@ document.getElementById("adminLink").addEventListener("click", function(event) {
     document.getElementById("adminForm").style.display = "block";
 });
 
+document.getElementById("closeFormBtn").addEventListener("click", function() {
+    document.getElementById("adminForm").style.display = "none";
+});
+
+document.getElementById("clearFormBtn").addEventListener("click", function() {
+    document.getElementById("loginForm").reset();
+});
+
+
+
